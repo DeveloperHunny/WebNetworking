cd32130da234f0b67e07a236bca0d8b469bd9e45
Hunny <eogns0321@gmail.com>
2021-05-09 13:41:47 +0900

    complete login module and home.php(update Module too)

--- home.php
+++ home.php
@@ -7,19 +7,68 @@
     <title>HomePage</title>
 
     <link rel="stylesheet" href="css/home_style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
+    <?php connectDB(); ?>
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $serialNum = (int)$_POST["serialNum"] + 1;
+            $id = $_POST["id"];
+            $name = $_POST["name"];
+            $pw = $_POST["pw"];
+
+            $sql = "UPDATE clients 
+            SET id = '$id' , pw = '$pw', name = '$name'
+            WHERE serialNum = '$serialNum'";
+
+            if($_SERVER['conn']->query($sql) === TRUE){
+                echo "<script>location.replace('home.php')</script>";
+            }
+            else{
+                echo $_SERVER['conn']->error;
+            }
+        }
+
+    ?>
+
     <div class="container">
         <header>
             <span class="title">Client DB</span>
         </header>
         <main>
             <table>
-                <tr><th>ID</th><th>NAME</th><th>PASSWORD</th></tr>
-                <?php connectDB(); ?>
+                <tr><th></th><th>ID</th><th>NAME</th><th>PASSWORD</th></tr>
+                <?php settingTable(); ?>
             </table>
         </main>
+
+
+
+        <div class="layerDim">
+            <div class="bgClass">
+
+            </div>
+            <div class="popUp">
+                <span class="title">User Info</span>
+                <form action="home.php" method="POST">
+                    
+                    <ul>
+                        <li><input type="text" name="serialNum" id="inputSerial" style="display:none"></li>
+                        <li><label for="inputID">ID </label> <input type="text" name="id" id="inputID"></li>
+                        <li><label>NAME </label> <input type="text" name="name"></li>
+                        <li><label>PW </label> <input type="text" name="pw"></li>
+                    </ul>
+                    <button type="submit" id="submitBtn">Modify</button>  
+                    
+                </form>
+                <button id="closeBtn">Close</button>
+                
+            </div>
+        </div>
     </div>
+
+    <script src="js/main.js"></script>
 </body>
 </html>
 
@@ -34,20 +83,23 @@
         $sql = "";
     
         //Connect Database
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        $_SERVER['conn'] = new mysqli($servername, $username, $password, $dbname);
     
         //Check db connection
-        if($conn->connect_error){
+        if($_SERVER['conn']->connect_error){
             die("Connection failed: " . $conn->connect_error);
         }
-        
-        $sql = "SELECT * FROM clients";
-        $result = $conn->query($sql);
+
+ }
+
+ function settingTable(){
+    $sql = "SELECT * FROM clients";
+        $result = $_SERVER['conn']->query($sql);
+        $row = $result->fetch_assoc(); //첫줄 root 계정 안 보이게
+        $num = 1;
         while($row = $result->fetch_assoc()){
-            echo "<tr><td>" . $row["id"] ."</td><td>" . $row["name"] . "</td><td>" . $row["pw"] . "</td></tr>";
+            echo "<tr><th>" . $num ."</th><td>" . $row["id"] ."</td><td>" . $row["name"] . "</td><td>" . $row["pw"] . "</td></tr>";
+            $num++;
         }
-
-  
-
  }
 ?>
